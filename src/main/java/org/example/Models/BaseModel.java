4fb2477884d1cbd7f1ea853dd743f9f60926496d
ramanaguru <ramanaguruloyola@gmail.com>
2023-11-22 19:51:01 +0530

    First Commit

--- src/main/java/org/example/Models/BaseModel.java
+++ src/main/java/org/example/Models/BaseModel.java
@@ -1,7 +1,12 @@
 package org.example.Models;
 
+import java.util.Date;
+
 public class BaseModel {
     private Long id;
+    private Date createdAt;
+
+    private Date updateddAt;
 
     public Long getId() {
         return id;
